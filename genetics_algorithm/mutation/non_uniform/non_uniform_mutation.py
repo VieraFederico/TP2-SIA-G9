11040551712893c7d4bf5e615a33d4e8605e43ca
felitba <feli@itba.edu.ar>
2026-04-10 02:42:34 -0300

    feat: impl non uniform mutation

--- genetics_algorithm/mutation/non_uniform/non_uniform_mutation.py
+++ genetics_algorithm/mutation/non_uniform/non_uniform_mutation.py
@@ -1,7 +1,44 @@
 from genetics_algorithm.mutation.mutation_method import MutationMethod
 from genetics_algorithm.models.Individual import Individual
-
+import random
 
 class NonUniformMutation(MutationMethod):
+
     def _tweak(self, individual: Individual, p_tweak: float):
-        raise NotImplementedError("NonUniformMutation is not yet implemented.")
+        if random.random() > self.p_tweak:
+            return None
+
+        # TODO: avoid code repetition for all mutations.
+        for polygon in individual.polygons:
+            if random.random() < 0.5:
+                # Pick a vertex from the polygon
+                vertex_idx = random.randint(0, 2)
+                x, y = polygon.vertices[vertex_idx]
+
+                # Shift the point by up to 15 pixels in any direction
+                delta_x = random.randint(-15, 15)
+                delta_y = random.randint(-15, 15)
+
+                # Ensure the new vertex doesn't go outside the canvas boundaries
+                new_x = max(0, min(individual.width, x + delta_x))
+                new_y = max(0, min(individual.height, y + delta_y))
+
+                # Update the polygon's vertices
+                new_vertices = list(polygon.vertices)
+                new_vertices[vertex_idx] = (new_x, new_y)
+                polygon.vertices = tuple(new_vertices)
+
+            else:
+                # Change color
+                channel_idx = random.randint(0, 3)
+                color = list(polygon.color)
+
+                # Shift the color value by up to 20 units
+                delta_c = random.randint(-20, 20)
+
+                # Ensure the color value stays between 0 and 255
+                new_c = max(0, min(255, color[channel_idx] + delta_c))
+
+                color[channel_idx] = new_c
+                polygon.color = tuple(color)
+        return None
