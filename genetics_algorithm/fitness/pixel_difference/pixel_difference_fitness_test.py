27b6ea89f92eb6846c37f8b1aee2b8cd6a27911c
Federico Viera <fedviera@gmail.com>
2026-04-14 00:35:19 -0300

    Fix: Deleten Unnecesary File

--- genetics_algorithm/fitness/pixel_difference/pixel_difference_fitness_test.py
+++ genetics_algorithm/fitness/pixel_difference/pixel_difference_fitness_test.py
@@ -2,36 +2,32 @@ import unittest
 from PIL import Image
 import numpy as np
 
-from genetics_algorithm.fitness.pixel_difference.mae import PixelDifferenceFitnessMAE
-from genetics_algorithm.fitness.pixel_difference.pixel_difference_fitness import _mse, _mae, _ssim, \
-    PixelDifferenceFitness
+from genetics_algorithm.fitness.pixel_difference.mae import PixelDifferenceFitnessMAE, _mae
+from genetics_algorithm.fitness.pixel_difference.mse import _mse
+from genetics_algorithm.fitness.pixel_difference.ssim import _ssim
 from genetics_algorithm.models import Individual
 
 
 class PixelDifferenceFitnessTest(unittest.TestCase):
     def test_no_difference_between_images_mse(self):
-        # setup images
         img1 = np.array(Image.new("RGBA", (3, 3), "white")).astype(np.float32)
         img2 = np.array(Image.new("RGBA", (3, 3), "white")).astype(np.float32)
         result = _mse(img1, img2)
         self.assertEqual(0.0, result)
 
     def test_no_difference_between_images_mae(self):
-        # setup images
         img1 = np.array(Image.new("RGB", (3, 3), "white")).astype(np.float32)
         img2 = np.array(Image.new("RGB", (3, 3), "white")).astype(np.float32)
         result = _mae(img1, img2)
         self.assertEqual(0.0, result)
 
     def test_no_difference_between_images_ssim(self):
-        # setup images
         img1 = np.array(Image.new("RGB", (3, 3), "white")).astype(np.float32)
         img2 = np.array(Image.new("RGB", (3, 3), "white")).astype(np.float32)
         result = _ssim(img1, img2)
         self.assertEqual(0.0, result)
 
     def test_opposite_colour_images_ssim(self):
-        # setup images
         img1 = np.array(Image.new("RGB", (3, 3), "white")).astype(np.float32)
         img2 = np.array(Image.new("RGB", (3, 3), "black")).astype(np.float32)
         result = _ssim(img1, img2)
@@ -40,30 +36,17 @@ class PixelDifferenceFitnessTest(unittest.TestCase):
     def test_partial_difference_mse(self):
         img1 = np.array([[0, 0],
                          [0, 0]]).astype(np.float32)
-
         img2 = np.array([[10, 0],
                          [0, 0]]).astype(np.float32)
-
         result = _mse(img1, img2)
         self.assertEqual(25.0, result)
 
     def test_evaluate_run(self):
         target_image = Image.open("resources/images/noche.png").convert("RGBA")
-        ind = Individual(
-            target_image.width,
-            target_image.height,
-            20,
-        )
-
-        pixel_difference_mae = PixelDifferenceFitnessMAE(target_image)
-        pixel_difference = PixelDifferenceFitness(target_image)
-        print("Start evaluating")
-        result = pixel_difference.evaluate(ind )
-        result_mae = pixel_difference_mae.evaluate(ind )
-        print(result)
-        print(result_mae)
-        print(f"end")
-        self.assertEqual(result_mae, result)
+        ind = Individual(target_image.width, target_image.height, 20)
+        fitness_fn = PixelDifferenceFitnessMAE(target_image)
+        result = fitness_fn.evaluate(ind)
+        self.assertLess(result, 0.0)
 
 
 if __name__ == '__main__':
